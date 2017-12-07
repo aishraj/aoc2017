0869a2ab2e49d735bd2a48141548527cdc793f79
Aish Raj Dahal <aish@aishraj.com>
2017-12-07 07:29:45 -0800

    Add solution for day 5 part 1

---
+++ five/src/main.rs
@@ -0,0 +1,45 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Read;
+
+fn main() {
+    let mut arr = read_into_vec("./input.txt");
+    let arr_size = arr.len();
+    let mut i = 0;
+    let mut c = 0;
+    while i < arr_size {
+        //println!("Array is {:?}", arr);
+        //      println!("i is: {:?}", i);
+
+        let t = arr[i];
+        arr[i] = arr[i] + 1;
+        if t < 0 {
+            let t_value = t.abs();
+            let t_value = t_value as usize;
+            i = i - t_value;
+        } else {
+            let t_value = t as usize;
+            i = i + t_value;
+        }
+        c += 1;
+        //println!("i is: {:?}", i);
+
+    }
+    //println!("Array is {:?}", arr);
+    println!("Number of steps is: {:?}", c);
+}
+
+
+fn read_into_vec(filename: &str) -> Vec<i32> {
+    let f = File::open(filename).expect("Unable to open the file");
+    let mut reader = BufReader::new(f);
+    let mut contents = String::new();
+    reader.read_to_string(&mut contents).expect(
+        "Unable to read file to string",
+    );
+    let items: Vec<i32> = contents
+        .split_whitespace()
+        .map(|num| num.parse::<i32>().unwrap())
+        .collect();
+    items
+}
